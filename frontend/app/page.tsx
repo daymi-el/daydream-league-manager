3581b8c5b7e30a079d2a43e64c9efa6458c97329
Daniel Kistner <kistnerdaniel@pm.me>
2026-01-14 15:17:06 +0100

    added AddAccount & AccountCard

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,39 +2,24 @@
 
 import {useAccounts} from "@/providers/accounts-provider";
 import {useRequests} from "@/hooks/use-requests";
-import {useEffect, useState} from "react";
-import {AccountDto} from "twisted/dist/models-dto/account";
-import {SummonerLeagueDto} from "twisted/dist/models-dto/league";
 import {AccountCard} from "@/components/blocks/account-card";
+import {AddAccountDialog} from "@/components/blocks/add-account-dialog";
 
 export default function Home() {
     const {addAccount, accounts} = useAccounts();
     const {getLeagueAccountData, getRiotAccount} = useRequests();
 
-    const [riotAcc, setRiotAcc] = useState<AccountDto>()
-    const [leagueAcc, setLeagueAcc] = useState<SummonerLeagueDto[]>()
-
-    useEffect(() => {
-        async function fetchData() {
-            const riotData = await getRiotAccount("day", "mii");
-            const leagueEntries = await getLeagueAccountData(riotData.puuid)
-
-            addAccount({
-                leagueData: leagueEntries,
-                userName: "",
-                riotData: riotData,
-                password: "",
-                gameName: riotData.gameName,
-                tagLine: riotData.tagLine
-            })
-        }
-
-        fetchData();
-    }, []);
-
     return (
         <div className="flex min-h-screen items-center justify-center">
-            {[...accounts.values()].map(acc => <AccountCard key={acc.userName} account={acc}/>)}
+            <div className={"absolute top-0 right-0 p-2"}>
+                <AddAccountDialog/>
+            </div>
+            <div className={"flex flex-col sm:flex-row gap-2"}>
+                {[...accounts.values()].map(acc => (
+                    <AccountCard key={acc.userName} account={acc}/>
+                ))}
+            </div>
+
         </div>
     );
 }
